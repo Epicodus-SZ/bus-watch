acb01a0dc33ce315b51fc9fef3a68fcde4f5bc2e
Joe Kramer <joekramer1125@gmail.com>
2017-08-31 10:13:04 -0700

    add all arrival times

--- src/app/watch-list-item/watch-list-item.component.ts
+++ src/app/watch-list-item/watch-list-item.component.ts
@@ -15,55 +15,53 @@ export class WatchListItemComponent implements OnInit {
 
   // newMatch: Watch = this.watch;
   data: any;
-  dataArray: any[] = [];
-  testTime: number = 0;
 
   constructor(private apiData: ApiDataService) {
-    setInterval(() => { this.getNextArrivalTime(this.watch.routeID); }, 5000);
+    // setInterval(() => { this.getNextArrivalTime(this.watch.routeID); }, 5000);
   }
 
   ngOnInit() {
     //open stream
-    this.apiData.apiCall(this.watch.stopID)
-    .subscribe(res => {
-        this.data = res;
-    });
+    // this.apiData.apiCall(this.watch.stopID)
+    // .subscribe(res => {
+    //     this.data = res;
+    // });
   }
 
 
-  getNextArrivalTime(routeId: string) {
-    this.apiData.apiCall(this.watch.stopID)
-    .subscribe(res => {
-        this.data = res;
-        this.dataArray.push(this.data);
-        console.log("res=",res);
-        console.log(this.dataArray);
-
-    });
-
-    console.log(this.data);
-    let currentTime: number = 0;
-    let time: number = 8888888888888888;
-    let counter: number = 0;
-
-    currentTime = this.data.currentTime;
-
-    this.data.data.entry.arrivalsAndDepartures.forEach(arrival => {
-      if((routeId === arrival.routeShortName) && (counter < 1)) {
-        if(arrival.scheduledArrivalTime > currentTime) {
-          counter++;
-          time = arrival.scheduledArrivalTime;
-        }
-      }
-    });
-
-    this.watch.nextArrival = (time - currentTime) / 1000 / 60;
-  }
+  // getNextArrivalTime(routeId: string) {
+  //   this.apiData.apiCall(this.watch.stopID)
+  //   .subscribe(res => {
+  //       this.data = res;
+  //       this.dataArray.push(this.data);
+  //       console.log("res=",res);
+  //       console.log(this.dataArray);
+  //
+  //   });
+  //
+  //   console.log(this.data);
+  //   let currentTime: number = 0;
+  //   let time: number = 8888888888888888;
+  //   let counter: number = 0;
+  //
+  //   currentTime = this.data.currentTime;
+  //
+  //   this.data.data.entry.arrivalsAndDepartures.forEach(arrival => {
+  //     if((routeId === arrival.routeShortName) && (counter < 1)) {
+  //       if(arrival.scheduledArrivalTime > currentTime) {
+  //         counter++;
+  //         time = arrival.scheduledArrivalTime;
+  //       }
+  //     }
+  //   });
+  //
+  //   this.watch.nextArrival = (time - currentTime) / 1000 / 60;
+  // }
 
   beginDeletingWatch(watchToDelete){
     if(confirm("Are you sure you want to delete this route?")){
       console.log("here " + watchToDelete.$key);
-      this.apiDataService.deleteWatch(watchToDelete);
+      this.apiData.deleteWatch(watchToDelete);
     }
   }
 }
--- src/app/watch/watch.component.ts
+++ src/app/watch/watch.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { FirebaseListObservable } from 'angularfire2/database';
 import { Watch } from '../watch';
 import { ApiDataService } from '../api-data.service';
+import { Observable } from 'rxjs/Observable';
 
 
 @Component({
@@ -13,12 +14,51 @@ import { ApiDataService } from '../api-data.service';
 export class WatchComponent implements OnInit {
 
   watches: Watch[] = [];
+  arrivals;
+  data: any;
+  currentTime;
+  watch;
 
   constructor(private apiData: ApiDataService) {
+    setInterval(() => { this.getNextArrivalTime(); }, 5000);
   }
 
   ngOnInit() {
-    this.apiData.getWatches().subscribe(res => this.watches = res);
+    this.apiData.getWatches().subscribe(res => {
+      this.watch = res[0];
+      this.watches = res;
+    });
+    this.apiData.apiCall("570")
+    .subscribe(res => {
+        this.handleData(res),
+        res => this.handleError(res);
+    });
   }
 
+  getNextArrivalTime() {
+    this.apiData.apiCall("570")
+    .subscribe(res => this.handleData(res), res => this.handleError(res));
+
+    this.watches.forEach(watch => {
+      let counter: number = 0;
+      this.arrivals.forEach(arrival => {
+        if((watch.routeID === arrival.routeShortName) && (counter < 1)) {
+          if(arrival.scheduledArrivalTime > this.currentTime) {
+            counter++;
+            watch.nextArrival = ((arrival.scheduledArrivalTime - this.currentTime) / 60000);
+          }
+        }
+      });
+    });
+  }
+
+  handleData(response) {
+    this.currentTime = response.currentTime;
+    this.arrivals = response.data.entry.arrivalsAndDepartures;
+  }
+
+  handleError(error) {
+    console.log('error:', error);
+    return Observable.throw(error);
+  }
 }
